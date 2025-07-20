1370b2271ddadb36c96ab4c85097e15249fd0e46
rohitsinghwho <rohiisingh8655@gmail.com>
2025-07-20 10:27:51 +0530

    module 1 css updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "./index.module.css"
 import { BrowserRouter } from 'react-router-dom'
+import "./index.module.css"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
